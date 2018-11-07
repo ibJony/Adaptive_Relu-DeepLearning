c11d800799f5c3f1662b0514f706dd40ed30090b
ningjize <ningjize@users.noreply.github.com>
2018-11-08 02:23:00 +0300

    Update SimpleAdaRelu.py

--- SimpleAdaRelu.py
+++ SimpleAdaRelu.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 
 
 # This just set a learnable relu threshold without sharing 
-class SimpleAdaptiveRelu(nn.Module):
+class AdaptiveRelu(nn.Module):
 	def __init__(self):
 		super(AdaptiveRelu, self).__init__()
 		self.thr = nn.Parameter(torch.tensor(0.0))
 	def forward(self, x):
 		x = nn.ReLU()(x - self.thr)
-		print(self.thr)
+# 		print(self.thr)
 		return x
